11d6e10e0a41aec20cddf12baf842fd7d73ed1b2
pmpyy <pmpyyyoyo2@gmail.com>
2023-10-04 11:52:05 +0700

    finish

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,16 +2,25 @@
 <template>
   <div class="htmlClass">
     <h1>{{ title }}</h1>
+    <InputPage></InputPage>
+    <FunctionPage></FunctionPage>
   </div>
 </template>
 
 
 <script>
+import InputPage from './InputPage.vue';
+import FunctionPage from './FunctionPage.vue'
 export default {
+
   name: 'HtmlComponent',
+  components:{
+    InputPage,
+    FunctionPage
+  },
   data() {
     return {
-      title: 'Vue is awesome.',
+      title: 'Pimonpaan',
     }
   },
 }
